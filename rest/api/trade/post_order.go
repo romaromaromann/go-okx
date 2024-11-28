36dbe99f7b27a1015bbc5b08e9901668cf700606
Roman <innirvana96@gmail.com>
2024-11-28 12:34:40 +0400

    28/11 12:30

--- rest/api/trade/post_order.go
+++ rest/api/trade/post_order.go
@@ -11,22 +11,22 @@ func NewPostOrder(param *PostOrderParam) (api.IRequest, api.IResponse) {
 }
 
 type PostOrderParam struct {
-	InstId     string `json:"instId"`
-	TdMode     string `json:"tdMode"`
-	Ccy        string `json:"ccy,omitempty"`
-	ClOrdId    string `json:"clOrdId,omitempty"`
-	Tag        string `json:"tag,omitempty"`
-	Side       string `json:"side"`
-	PosSide    string `json:"posSide,omitempty"`
-	OrdType    string `json:"ordType"`
-	Sz         string `json:"sz"`
-	Px         string `json:"px,omitempty"`
-	ReduceOnly bool   `json:"reduceOnly,omitempty"`
-	TgtCcy     string `json:"tgtCcy,omitempty"`
-	TPTriggerPx string `json:"tpTriggerPx,omitempty,string"` 
-	TPOrdPx     string `json:"tpOrdPx,omitempty,string"`     
-	SLTriggerPx string `json:"slTriggerPx,omitempty,string"` 
-	SLOrdPx     string `json:"slOrdPx,omitempty,string"`     
+	InstId      string  `json:"instId"`
+	TdMode      string  `json:"tdMode"`
+	Ccy         string  `json:"ccy,omitempty"`
+	ClOrdId     string  `json:"clOrdId,omitempty"`
+	Tag         string  `json:"tag,omitempty"`
+	Side        string  `json:"side"`
+	PosSide     string  `json:"posSide,omitempty"`
+	OrdType     string  `json:"ordType"`
+	Sz          string  `json:"sz"`
+	Px          string  `json:"px,omitempty"`
+	ReduceOnly  bool    `json:"reduceOnly,omitempty"`
+	TgtCcy      string  `json:"tgtCcy,omitempty"`
+	TPTriggerPx string  `json:"tpTriggerPx,omitempty,string"`
+	TPOrdPx     string  `json:"tpOrdPx,omitempty,string"`
+	SLTriggerPx string  `json:"slTriggerPx,omitempty,string"`
+	SLOrdPx     float64 `json:"slOrdPx,omitempty,string"`
 }
 
 type PostOrderResponse struct {
