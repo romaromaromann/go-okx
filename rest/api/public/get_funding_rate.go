9c52276b3043a5a2811e4503c3db288edf7a6a9a
Roman <innirvana96@gmail.com>
2025-04-02 12:25:26 +0400

    added public/fundingRate

---
+++ rest/api/public/get_funding_rate.go
@@ -0,0 +1,28 @@
+package public
+
+import "github.com/romaromaromann/go-okx/rest/api"
+
+func NewGetFundingRate(param *GetFundingParam) (api.IRequest, api.IResponse) {
+	return &api.Request{
+		Path:   "/api/v5/public/funding-rate",
+		Method: api.MethodGet,
+		Param:  param,
+	}, &GetFundingResponse{}
+
+}
+
+type GetFundingParam struct {
+	InstId string `url:"instId"`
+}
+
+type GetFundingResponse struct {
+	api.Response
+	Data []Funding `json:"data"`
+}
+
+type Funding struct {
+	FundingRate     string `json:"fundingRate"`
+	FundingTime     string `json:"fundingTime"`
+	InstId          string `json:"instId"`
+	NextFundingTime string `json:"nextFundingTime"`
+}
